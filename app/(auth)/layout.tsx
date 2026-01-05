9ca097b8de9c8e0b2fc0e3ef77d7903d2e8ddb3b
student <student@aprotrain-aptech.edu>
2026-01-05 10:58:04 +0700

    hshs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-
+import Image from "next/image";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
@@ -8,15 +8,20 @@ export default function Layout({ children }: { children: ReactNode }) {
           relative
         min-h-[100svh]
         w-full
-        bg-[url('https://res.cloudinary.com/dcfaz2rme/image/upload/v1767585061/wallhaven_opqtwn.jpg')]
-        bg-cover
-        bg-center
-        bg-no-repeat
+       
         flex
         items-center
         justify-center
       "
       >
+        <Image
+          src="https://res.cloudinary.com/dcfaz2rme/image/upload/v1767585061/wallhaven_opqtwn.jpg"
+          alt="Background"
+          fill
+          priority
+          sizes="(max-width: 768px) 100vw, 50vw"
+          className="object-cover"
+        />
         <div className="absolute inset-0 bg-black/30 z-0" />
         <div className="relative z-10 flex items-center justify-center w-full p-5 md:p-9">
           <div className="w-full max-w-sm md:max-w-md lg:max-w-lg">{children}</div>
