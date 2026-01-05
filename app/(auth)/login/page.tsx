020ff1e3dee7a5d6ee79788d062f620f4d7e06c3
student <student@aprotrain-aptech.edu>
2026-01-05 10:43:40 +0700

    ass

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,10 +2,12 @@ import LoginPage from "@/components/pages/user/LoginPage";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function Page() {
 
-  return
-  <div>
-    <LoginPage />;
-    <SpeedInsights></SpeedInsights>
-  </div>
+  return (
+    <div>
+      <LoginPage />;
+      <SpeedInsights></SpeedInsights>
+    </div>
+
+  )
 
 }
